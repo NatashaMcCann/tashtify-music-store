88ba2868caef8a6ba44e9f00b425de569fa4de02
Natasha McCann <natasha.mccann@ft.com>
2019-10-18 16:57:21 +0100

    Added fixes for List Albums, Add Album, Update Album, Delete Album

--- music_store.py
+++ music_store.py
@@ -1,8 +1,3 @@
-# 1. Convert albums from a dict {} to a list []
-# 2. Model an album as a dict { 'name': 'Holy Fire', 'artist': 'Foals'}
-# intialize  function that saves the albums and prepopulates the albums
-
-
 albums = [
     {
         "Artist": "Foals",
@@ -25,64 +20,47 @@ menu = {
 }
 
 
+#DONE
 def list_albums():
     for album in albums:
         print(album["Name"],'-', album["Artist"])
 
 
+#DONE
 def add_album():
     album_name = input("Please enter an album name: ")
     artist = input("Please enter the artist: ")
     albums.append({"Artist": artist, "Name": album_name})
 
 
+#DONE
 def update_album():
-    # 1) Ask user for the name of the album to update.
     album_name_to_find = input("Which album would you like to update?: ")
-
-    # 2) See if album exists in music store.
     album_names = [album["Name"] for album in albums]
     if album_name_to_find in album_names:
-        print("Found Album")
-
-
-
+        position = album_names.index(album_name_to_find)
+        del albums[position]
+        album_name_to_update = input("Please enter the update to the album: ")
+        artist_to_update = input("Please enter the update to the artist: ")
+        albums.append({"Artist": artist_to_update, "Name": album_name_to_update})
     else:
         print("Didn't find album")
 
 
-    # 3) If album does exist, then ask user for the update they would like to make to the album name.
-    # 4) If the album does not exist, tell the user, album does not exist in store.
-
-
-    # if album_update_question in albums[values]:
-    #     position = album_names.index(album_update_question)
-    #     del albums[position]
-    #     album_update_modify = input("Please enter the update you would like to make: ")
-    #     albums.insert(position, (album_update_modify))
-    # else:
-    #     print("Album does not exist")
-
-
-# def delete_album():
-#     album_delete = input("Which album would you like to delete?: ")
-#     if album_delete i
-#         position = album_names.index(album_delete)
-#         del albums[position]
-#     else:
-#         print("Specified album not in stock")
+#DONE
+def delete_album():
+    album_delete = input("Which album would you like to delete?: ")
+    album_names = [album["Name"] for album in albums]
+    if album_delete in album_names:
+        position = album_names.index(album_delete)
+        del albums[position]
+    else:
+        print("Album is not in list")
 
 
 def albums_by_artist():
     artist = input("Please enter the artist: ")
-    for key,value in dict in albums:
-        if value == artist:
-            print(value)
-        else:
-            print("Specified album not in stock")
-    # if artists exist as key in albums
-    # retrieve value of the artist
-    # if not, print a string saying this inputted artist is not currently in stock.
+  
 
 
 def show_menu():
